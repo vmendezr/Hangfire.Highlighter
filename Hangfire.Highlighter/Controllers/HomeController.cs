cb5849461a200b7f938bf1ef26d50e13626db1a9
victor <victormendezromero@gmail.com>
2016-03-07 21:35:12 +0100

    Configured EntityFramework and MiniProfiler. App working.

--- Hangfire.Highlighter/Controllers/HomeController.cs
+++ Hangfire.Highlighter/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Hangfire.Highlighter.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +11,92 @@ namespace Hangfire.Highlighter.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        private readonly HighlighterDbContext _db = new HighlighterDbContext();
+
         public ActionResult Index()
         {
+            return View(_db.CodeSnippets.ToList());
+        }
+
+        public ActionResult Details(int id)
+        {
+            var snippet = _db.CodeSnippets.Find(id);
+            return View(snippet);
+        }
+
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "SourceCode")] CodeSnippet snippet)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    snippet.CreatedAt = DateTime.UtcNow;
+
+                    using (StackExchange.Profiling.MiniProfiler.StepStatic("Service call"))
+                    {
+                        snippet.HighlightedCode = HighlightSource(snippet.SourceCode);
+                        snippet.HighlightedAt = DateTime.UtcNow;
+                    }
+
+                    _db.CodeSnippets.Add(snippet);
+                    _db.SaveChanges();
+
+                    return RedirectToAction("Details", new { id = snippet.Id });
+                }
+
+            }
+            catch (HttpRequestException)
+            {
+                ModelState.AddModelError("", "Highlighting service returned error. Try again later.");
+            }
+
+            return View(snippet);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private static async Task<string> HighlightSourceAsync(string source)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync(
+                    @"http://hilite.me/api",
+                    new FormUrlEncodedContent(new Dictionary<string, string>
+                    {
+                    { "lexer", "c#" },
+                    { "style", "vs" },
+                    { "code", source }
+                    }));
+
+                response.EnsureSuccessStatusCode();
+
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+
+        private static string HighlightSource(string source)
+        {
+            // Microsoft.Net.Http does not provide synchronous API,
+            // so we are using wrapper to perform a sync call.
+            return RunSync(() => HighlightSourceAsync(source));
+        }
+
+        private static TResult RunSync<TResult>(Func<Task<TResult>> func)
+        {
+            return Task.Run<Task<TResult>>(func).Unwrap().GetAwaiter().GetResult();
         }
     }
 }
--- Hangfire.Highlighter/Models/HighlighterDbContext .cs
+++ Hangfire.Highlighter/Models/HighlighterDbContext .cs
@@ -6,10 +6,13 @@ using System.Web;
 
 namespace Hangfire.Highlighter.Models
 {
-    public class HighlighterDbContext: DbContext
+    public class HighlighterDbContext : DbContext
     {
+        public DbSet<CodeSnippet> CodeSnippets { get; set; }
+
         public HighlighterDbContext() : base("HighlighterDb")
         {
+            
         }
     }
 }
